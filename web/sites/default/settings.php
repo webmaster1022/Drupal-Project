7751bdef35bde7786edbc1fa925189e0a84ea704
webmaster1022 <104451862+webmaster1022@users.noreply.github.com>
2022-07-23 22:50:28 +0200

    Edited the setting.php for env.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -67,9 +67,6 @@ switch ($env) {
     $config['system.performance']['css']['preprocess'] = FALSE;
     $config['system.performance']['js']['preprocess'] = FALSE;
     $settings['cache']['bins']['render'] = 'cache.backend.null';
-    $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
-    $settings['cache']['bins']['page'] = 'cache.backend.null';
-    $settings['extension_discovery_scan_tests'] = FALSE;
     break;
 
   default:
